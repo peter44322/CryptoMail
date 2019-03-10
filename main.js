3b47c20e7cbb5300b663ff9cefa14f4cb959e96f
unknown <p.pator@outlook.com>
2019-03-10 17:29:16 +0200

    express & front end

--- main.js
+++ main.js
@@ -8,9 +8,16 @@ const { Connection } = require("./lib/Connection");
 const { User } = require("./lib/User");
 var user = new User();
 var connection = new Connection(user);
+
+
+
+
 app.use(express.static('public'));
 app.use(expressEdge);
-//user.id.toString("hex")
-app.get("/", (req, res) => res.render('index'));
+app.get("/", (req, res) =>{
+let id =user.id.toString("hex")
+
+   res.render('index',{id})
+ });
 
 app.listen(port, () => console.log(`listening on port ${port}!`));
